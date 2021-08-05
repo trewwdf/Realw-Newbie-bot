bb402d341d1d0b405e3b03ca561b6b7f68c8fb49
대권 <rlaeornjs55@gmail.com>
2021-08-05 11:06:34 +0900

    Update ready.py

--- realw_newbie/cogs/ready.py
+++ realw_newbie/cogs/ready.py
@@ -9,6 +9,10 @@ class ready(commands.Cog):
         print(self.bot.user)
         print(self.bot.user.id)
         print('Newbie BOT has Ready.')
-
+        
+    @commands.Cog.listener()
+        async def on_command_error(self, ctx, error):
+            if isinstance(error, commands.CommandNotFound):
+                pass
 def setup(bot):
     bot.add_cog(ready(bot))
