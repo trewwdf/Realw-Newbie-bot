a1176616e83e2d914927012894e7084ebf1bf1dc
대권 <rlaeornjs55@gmail.com>
2021-08-09 23:29:07 +0900

    Update __main__.py

--- realw_newbie/__main__.py
+++ realw_newbie/__main__.py
@@ -7,7 +7,7 @@ try:
     load_extension()
     bot.run(token)
 except discord.LoginFailure as e:
-    ctypes.windll.user32.MessageBoxW(0, f"토큰을 제대로 적어주세요!\n\n{e}\n\n확인을 누르시면 토큰을 얻는 방법 웹 브라우저가 열립니다.", "Error", 0)
+    ctypes.windll.user32.MessageBoxW(0, f"토큰을 제대로 적어주세요!\n\n{e}\n\n확인을 누르시면 토큰 발급 방법 웹 브라우저가 열립니다.", "Error", 0)
     webbrowser.open('https://github.com/Tyrrrz/DiscordChatExporter/wiki/Obtaining-Token-and-Channel-IDs')
     quit()
 except discord.PrivilegedIntentsRequired as e:
