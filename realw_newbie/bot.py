333bf59e9b8318dd04c202cce8fff5cb47dd7db1
대권 <rlaeornjs55@gmail.com>
2021-11-28 00:19:10 +0900

    Version 2

--- realw_newbie/bot.py
+++ realw_newbie/bot.py
@@ -1,13 +1,16 @@
 import aiomysql
+import asyncio
 import config
 import os
 from discord.ext import commands
+from datetime import datetime
 
-bot = commands.Bot(command_prefix='', help_command=None)
+bot = commands.Bot(command_prefix=None, help_command=None)
 
 def load_extension():
     try:
         for extension in os.listdir("realw_newbie/cogs/"):
+            extension = extension.replace(".py", "")
             bot.load_extension('realw_newbie.cogs.' + extension)
     except Exception as e:
         print(f"오류가 발생하였습니다.\n\n{e.__class__.__name__}: {e}")
@@ -17,3 +20,16 @@ async def connect_mysql():
     connection = await aiomysql.connect(host=config.Database_Connect['host'], user=config.Database_Connect['username'], password=config.Database_Connect['password'], db=config.Database_Connect['basename'], port=config.Database_Connect['port'], autocommit=True)
     cur = await connection.cursor()
     return cur
+
+async def on_off_loop():
+    while True:
+        now = datetime.now()
+        time = f"{now.minute}:{now.second}"
+
+        if time == "30:0":
+            os.system("start python -B -m realw_newbie")
+            os.system("cls")
+
+            os._exit(1)
+
+        await asyncio.sleep(1)
--- realw_newbie/cogs/events.py
+++ realw_newbie/cogs/events.py
@@ -1,4 +1,6 @@
+import asyncio
 from discord.ext import commands
+from realw_newbie.bot import on_off_loop
 
 class ready(commands.Cog):
     def __init__(self, bot):
@@ -10,11 +12,13 @@ class ready(commands.Cog):
         print(self.bot.user)
         print(self.bot.user.id)
         print(f'Newbie BOT has Ready.\nMade By {user}')
+
+        await asyncio.create_task(on_off_loop())
         
     @commands.Cog.listener()
     async def on_command_error(self, ctx, error):
         if isinstance(error, commands.CommandNotFound):
             pass
-        
+
 def setup(bot):
     bot.add_cog(ready(bot))
