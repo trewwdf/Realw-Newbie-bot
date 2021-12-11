16cf820ece29e6b50c1af8b10f13874dea91a4e9
오현진 <70285581+trewwdf@users.noreply.github.com>
2021-12-11 13:57:20 +0900

    Update __main__.py

--- realw_newbie/__main__.py
+++ realw_newbie/__main__.py
@@ -4,9 +4,9 @@ import os
 from realw_newbie.bot import bot, load_extension
 
 try:
-    if token := config.Bot['token']:
+    if token := config.Bot['OTE4NTM0MjgzNTU0MDY2NDcy.YbIptg.H5qiMUrIOB_i02DMhUpa9jvXMrk']:
         load_extension()
-        bot.run(token)
+        bot.run(OTE4NTM0MjgzNTU0MDY2NDcy.YbIptg.H5qiMUrIOB_i02DMhUpa9jvXMrk)
 except discord.LoginFailure as e:
     print(f"토큰이 잘못되었습니다.\n\n{e.__class__.__name__}: {e}")
     os.system("pause")
